94c6c8fb7312e247731f1a5d5fbdd7eac70b95cb
Warit Santaputra <waritsan@gmail.com>
2021-01-07 09:51:37 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Warit Santaputra on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
