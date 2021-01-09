37afd5034d4449dafa3822bb467c3583686c15a5
Warit Santaputra <waritsan@gmail.com>
2021-01-09 11:14:43 +0700

    Add ViewModel

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,5 +5,27 @@
 //  Created by Warit Santaputra on 8/1/21.
 //
 
-import Foundation
+import SwiftUI
  
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
